ee641cc5fe4f298398733457763385bd24093540
atomicoo <atomicoo95@gmail.com>
2020-09-12 20:19:01 +0800

    Update content about 'LEARNING PYTORCH'

--- LEARNING_PYTORCH/WHAT_IS_TORCH_NN.py
+++ LEARNING_PYTORCH/WHAT_IS_TORCH_NN.py
@@ -27,18 +27,38 @@ import torch.optim as optim
 ########################################
 ## C O N T E N T S
 ## 
-## @@ torch.nn
-## @@ torch.optim
-## @@ Dataset
-## @@ DataLoader
+## @@ MNIST data setup
+## @@ Neural Net from scratch (no torch.nn)
+## @@ Using torch.nn.functional
+## @@ Refactor using nn.Module
+## @@ Refactor using nn.Linear
+## @@ Refactor using optim
+## @@ Refactor using Dataset
+## @@ Refactor using DataLoader
+## @@ The whole final code
+## @@ Switch to CNN
+## @@ nn.Sequential
+## @@ Wrapping DataLoader
+## @@ Using your GPU
 ## 
 ## 
+
+# torch.nn
+#   - Module：包含状态（譬如神经网络权重）的可调用函数，大致对应神经网络中的Layers
+#   - Parameter：Tensor的高一层封装，表示Module的参数
+#   - functional：包括损失函数、激活函数以及各种神经网络Layers的无状态版本
+# torch.optim
+#   包括各种优化器，在反向传播时更新权重Parameter
+# Dataset
+#   具有`__len__`和`__getitem__`方法的抽象接口
+# DataLoader
+#   联合Dataset和Sampler，从Dataset中创建迭代器，每次返回一个批次的数据
 
 ## @@@@@@@@@@@@@@@@@@@
 ## MNIST data setup
 
 # %%
-# 使用pathlib包处理路径
+# 使用pathlib包处理路径（赞）
 from pathlib import Path
 # 使用requests下载数据
 import requests
@@ -146,7 +166,7 @@ print(accuracy(outputs, targets))
 
 # %%
 # 训练Loops
-from IPython.core.debugger import set_trace
+# from IPython.core.debugger import set_trace
 
 lr = 0.5  # 学习率
 epochs = 2  # 迭代次数
@@ -244,11 +264,89 @@ opt.zero_grad()
 ## Refactor using Dataset
 
 # %%
+# PyTorch提供抽象的`Dataset`类。
+# 可以通过子类化`torch.utils.data.Dataset`来快速构建自定义数据集
+# 必须具有`__len__()`方法和`__getitem__()`方法
 from torch.utils.data import TensorDataset
 
+train_ds = TensorDataset(x_train, y_train)
+# valid_ds = TensorDataset(x_valid, y_valid)
+
+inputs, targets = train_ds[bgn:end]
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## Refactor using DataLoader
 
 # %%
-import math
+# `DataLoader`负责数据的载入，主要为批次的管理
+# 联合了`Dataset`和`Sampler`，前者为数据来源，后者为提取策略
+from torch.utils.data import DataLoader
+
+train_dl = DataLoader(dataset=train_ds, batch_size=B, shuffle=True)
+# valid_dl = DataLoader(dataset=valid_ds, batch_size=B*2)
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## Add validation
+
+# %%
+# 需要验证集来确保训练过程不会过拟合
+# 通过`.train()`和`.eval()`切换模式来确保某些特殊Layers在不同模式下操作的正确性
+# 譬如，`nn.BatchNorm2D`或`nn.Dropout`
+valid_ds = TensorDataset(x_valid, y_valid)
+valid_dl = DataLoader(dataset=valid_ds, batch_size=B*2)
+
+model.eval()
+with torch.no_grad():
+    valid_loss = sum(loss_fn(model(inputs), targets) 
+                     for inputs, targets in valid_dl)
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## The whole final code
+
+# %%
+impoort math
+import pickle
+import gzip
+import requests
+from pathlib import Path
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+
+
+DATA_PATH = Path("data")
+PATH = DATA_PATH / "mnist"
+
+PATH.mkdir(parents=True, exist_ok=True)
+
+URL = "http://deeplearning.net/data/mnist/"
+FILENAME = "mnist.pkl.gz"
+
+if not (PATH / FILENAME).exists():
+    content = requests.get(URL+FILENAME).content
+    (PATH / FILENAME).open('wb').write(content)
+
+with gzip.open((PATH / FILENAME), 'rb') as f:
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
+
+
+# 转换为torch.tensor格式
+x_train, y_train, x_valid, y_valid = map(
+    torch.tensr, (x_train, y_train, x_valid, y_valid)
+)
+
+def get_data(train_ds, valid_ds, batch_size):
+    return (
+        DataLoader(dataset=train_ds, batch_size=batch_size, shuffle=True),
+        DataLoader(dataset=valid_ds, batch_size=batch_size*2),
+    )
 
 # 定义模型
 class Mnist_Logistic(nn.Module):
@@ -272,32 +370,199 @@ def accuracy(outputs, targets):
     predictions = torch.argmax(outputs, dim=-1)
     return (predictions == targets).float().mean()
 
-# 训练Loops
-B = 64  # 批次尺寸
-lr = 1e-3  # 学习率
-epochs = 2  # 迭代次数
-
-# 获取模型
-model, opt = get_model(Mnist_Logistic, lr)
-
-# 训练Loops
-for epoch in range(epochs):
-    for i in range((N-1)//B+1):
-        bgn = i * B
-        end = bgn + B
-        inputs = x_train[bgn:end]
-        targets = y_train[bgn:end]
-        outputs = model(inputs)
-        loss = loss_fn(outputs, targets)
-        acc = accuracy(outputs, targets)
+# 批次训练/评估
+def run_batch(model, loss_fn, inputs, targets, opt=None):
+    outputs = model(inputs)
+    loss = loss_fn(outputs, targets)
+    acc = accuracy(outputs, targets)
+    if opt is not None:
         loss.backward()
         opt.step()
         opt.zero_grad()
-        if (i+1) % 100 == 0:
-            print("## Epoch {:2d}, Step {:5d}, Loss {:.4f}, Acc {:.2%}"
-                .format(epoch, end, loss, acc))
+    return loss.item(), acc.item(), len(inputs)
+
+def fit(epochs, model, opt, train_dl, valid_dl):
+    # 训练Loops
+    for epoch in range(epochs):
+        model.train()
+        for i, (inputs, targets) in enumerate(train_dl):
+            loss, acc, num = run_batch(model, loss_fn, inputs, targets, opt)
+            if (i+1) % 100 == 0:
+                print("## Epoch {:2d}: Step {:5d}, Train Loss {:.4f}, Train Acc {:.2%}"
+                    .format(epoch, i*B, loss, acc))
+        model.eval()
+        with torch.no_grad():
+            losses, accs, nums = zip(
+                *[run_batch(model, loss_fn, inputs, targets) for inputs, targets in valid_dl]
+            )
+            valid_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
+            valid_acc = np.sum(np.multiply(accs, nums)) / np.sum(nums)
+        print("## Epoch {:2d}: Valid Loss {:.4f}, Valid Acc {:.2%}"
+            .format(epoch, valid_loss, valid_acc))
+
+B = 64  # 批次尺寸
+lr = 0.5  # 学习率
+epochs = 2  # 迭代次数
+
+# 创建数据集
+train_ds = TensorDataset(x_train, y_train)
+valid_ds = TensorDataset(x_valid, y_valid)
+
+# 数据载入
+train_dl, valid_dl = get_data(train_ds, valid_ds, B)
+# 获取模型
+model, opt = get_model(Mnist_Logistic, lr)
+# 训练模型
+fit(epochs, model, opt, train_dl, valid_dl)
+
+print(loss_fn(model(inputs), targets), accuracy(model(inputs), targets))
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## Switch to CNN
+
+# 可以看到，在利用PyTorch中已封装好的包对原代码进行重构之后，训练代码非常简洁
+# 同时训练代码与网络模型的解耦，使模型的切换变得非常方便
 
 # %%
+# 转换为卷积神经网络模型
+class Mnist_CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)
+    
+    def forward(self, inputs):
+        inputs = inputs.view(-1, 1, 28, 28)
+        inputs = F.relu(self.conv1(inputs))
+        inputs = F.relu(self.conv2(inputs))
+        inputs = F.relu(self.conv3(inputs))
+        inputs = F.avg_pool2d(inputs, 4)
+        return inputs.view(-1, inputs.size(1))
+
+lr = 0.1
+
+model = Mnist_CNN()
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+fit(epochs, model, opt, train_dl, valid_dl)
+
+print(loss_fn(model(inputs), targets), accuracy(model(inputs), targets))
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## nn.Sequential
+
+# %%
+from collections import OrderedDict
+
+# 根据函数返回相应的神经网络Layers
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+    
+    def forward(self, inputs):
+        return self.func(inputs)
+
+# 利用`torch.nn.Sequencial`快速编写自定义神经网络
+model = nn.Sequential(OrderedDict([
+    ('iview', Lambda(lambda x: x.view(-1, 1, 28, 28))),
+    ('conv1', nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu1', nn.ReLU()),
+    ('conv2', nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu2', nn.ReLU()),
+    ('conv3', nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)),
+    ('relu3', nn.ReLU()),
+    ('pool', nn.AvgPool2d(4)),
+    ('oview', Lambda(lambda x: x.view(-1, x.size(1)))),
+]))
+
+lr = 0.1
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+fit(epochs, model, opt, train_dl, valid_dl)
+
+print(loss_fn(model(inputs), targets), accuracy(model(inputs), targets))
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## Wrapping DataLoader
+
+# %%
+preprocess = lambda x, y: (x.view(-1, 1, 28, 28), y)
+
+# 封装DataLoader
+class WrappingDataLoader:
+    def __init__(self, dl, fn):
+        self.dl = dl
+        self.fn = fn
+    
+    def __len__(self):
+        return len(self.dl)
+    
+    def __iter__(self):
+        batches = iter(self.dl)
+        for b in batches:
+            yield self.fn(*b)
+
+train_dl, valid_dl = get_data(train_ds, valid_ds, B)
+train_dl = WrappingDataLoader(train_dl, preprocess)
+valid_dl = WrappingDataLoader(valid_dl, preprocess)
+
+# 利用`torch.nn.Sequencial`快速编写自定义神经网络
+model = nn.Sequential(OrderedDict([
+    ('conv1', nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu1', nn.ReLU()),
+    ('conv2', nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu2', nn.ReLU()),
+    ('conv3', nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)),
+    ('relu3', nn.ReLU()),
+    ('pool', nn.AdaptiveAvgPool2d(1)),
+    ('oview', Lambda(lambda x: x.view(-1, x.size(1)))),
+]))
+
+lr = 0.1
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+fit(epochs, model, opt, train_dl, valid_dl)
+
+inputs, targets = inputs.view(-1, 1, 28, 28), targets
+print(loss_fn(model(inputs), targets), accuracy(model(inputs), targets))
+
+
+## @@@@@@@@@@@@@@@@@@@@@@@@@@
+## Using your GPU
+
+# %%
+print(torch.cuda.is_available())
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+print(device)
+
+# %%
+preprocess = lambda x, y: (x.view(-1, 1, 28, 28).to(device), y.to(device))
+
+train_dl, valid_dl = get_data(train_ds, valid_ds, B)
+train_dl = WrappingDataLoader(train_dl, preprocess)
+valid_dl = WrappingDataLoader(valid_dl, preprocess)
+
+# 利用`torch.nn.Sequencial`快速编写自定义神经网络
+model = nn.Sequential(OrderedDict([
+    ('conv1', nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu1', nn.ReLU()),
+    ('conv2', nn.Conv2d(16, 16, kernel_size=3, stride=2, padding=1)),
+    ('relu2', nn.ReLU()),
+    ('conv3', nn.Conv2d(16, 10, kernel_size=3, stride=2, padding=1)),
+    ('relu3', nn.ReLU()),
+    ('pool', nn.AdaptiveAvgPool2d(1)),
+    ('oview', Lambda(lambda x: x.view(-1, x.size(1)))),
+]))
+
+lr = 0.1
+model = model.to(device)
+opt = optim.SGD(model.parameters(), lr=lr, momentum=0.9)
+fit(epochs, model, opt, train_dl, valid_dl)
+
+# %%
+inputs, targets = inputs.view(-1, 1, 28, 28).to(device), targets.to(device)
 print(loss_fn(model(inputs), targets), accuracy(model(inputs), targets))
 
 # %%
